f771c5cf5d13df8b67fa2300b8b1ec8e5f604103
reniohh <renohhreni@gmail.com>
2023-06-13 10:30:39 +0800

    membuaqt class main dan menjalankan program

--- src/Geometri/lingkaran.java
+++ src/Geometri/lingkaran.java
@@ -3,5 +3,17 @@ package Geometri;
 
 
 public class lingkaran {
-    
+     private double radius;
+
+    public Lingkaran(double radius) {
+        this.radius = radius;
+    }
+
+    public double luas() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public double keliling() {
+        return 2 * Math.PI * radius;
+    }
 }
--- src/Geometri/persegipanjang.java
+++ src/Geometri/persegipanjang.java
@@ -3,5 +3,19 @@ package Geometri;
 
 
 public class persegipanjang {
-    
+    private double panjang;
+    private double lebar;
+
+    public Persegipanjang(double panjang, double lebar) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
+
+    public double luas() {
+        return panjang * lebar;
+    }
+
+    public double keliling() {
+        return 2 * (panjang + lebar);
+    }
 }
